ad35574ca3bf65e53f6bbb366f06dccb38cd831e
Abdul Qaiyum Lee <9abdulqaiyum_lee@SIAQ00310MB.local>
2025-06-24 15:26:09 +0800

    Added more arguments

--- pdf_processor.py
+++ pdf_processor.py
@@ -6,20 +6,21 @@ from splitter import PDFSplitter
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", type=str, help="Path to input PDF file")
-    parser.add_argument("--output-dir", default="processed", help="Output directory for PNG files (optional)")
+    parser.add_argument("filepath", type=str, help="Path to input PDF file")
+    parser.add_argument("--pages-per-group", type=int, default=4, help="The number of pdf pages in each png file (default: 4) (optional)")
+    parser.add_argument("--output-dir", type=str, default="processed", help="Output directory for PNG files (optional)")
     args = parser.parse_args()
 
     redactor = PDFRedactor()
     splitter = PDFSplitter()
 
-    if not os.path.exists(args.filename):
+    if not os.path.exists(args.filepath):
         print(f"Error: Input file does not exist: {args.input_pdf}")
         return
     
     try:
-        outpath = redactor.redact_pdf(args.filename)
-        splitter.split_pdf(outpath, output_dir=args.output_dir)
+        outpath = redactor.redact_pdf(args.filepath)
+        splitter.split_pdf(outpath, pages_per_group=args.pages_per_group, output_dir=args.output_dir)
 
     except Exception as e:
         print(e)
